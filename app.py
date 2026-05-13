4da54e157c8ed554755ade3220f20aa8c3f21674
Kashika3006 <kashika3006@gmail.com>
2026-05-13 13:14:24 +0530

    fix module import for deployment

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import os 
 import numpy as np
 import pandas as pd
-from mlProject.pipeline.predictions import PredictionPipeline
+from src.mlProject.pipeline.predictions import PredictionPipeline
 
 
 app = Flask(__name__) # initializing a flask app
